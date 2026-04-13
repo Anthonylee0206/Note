14e52401ce00a41bdd506a3e2258d2bef28fc601
Anthonylee0206 <anthonylee0206@gmail.com>
2026-04-13 16:47:23 +0800

    auto backup_full.sql

--- Auto backup_full.sql
+++ Auto backup_full.sql
@@ -1,7 +1,7 @@
 SET NOCOUNT ON;
 
-DECLARE @DBPath   NVARCHAR(100) = N'D:\BAK';
-DECLARE @DBName     NVARCHAR(100)
+DECLARE @DBPath     NVARCHAR(100) = N'D:\BAK';
+DECLARE @DBName     NVARCHAR(100);
 DECLARE @BackupPath NVARCHAR(MAX);
 DECLARE @SQL        NVARCHAR(MAX);
 DECLARE @Mes        NVARCHAR(200);
@@ -12,13 +12,13 @@ INSERT INTO @DBList (DBName)
 SELECT [name]
 FROM sys.databases
 WHERE [name] NOT IN ('master', 'model', 'msdb', 'tempdb')
-	AND state_desc = 'ONLINE';
+    AND state_desc = 'ONLINE';
 
 WHILE EXISTS (SELECT 1 FROM @DBList)
 BEGIN
     SELECT TOP 1 @DBName = DBName FROM @DBList;
-	SELECT @Mes = N'---' + @DBName + N' BEGIN---';
-	RAISERROR(@Mes, 10, 1) WITH NOWAIT;
+    SELECT @Mes = N'---' + @DBName + N' BEGIN---';
+    RAISERROR(@Mes, 10, 1) WITH NOWAIT;
     
     SET @BackupPath = @DBPath + N'\' + @DBName + N'_full_' + FORMAT(GETDATE(), 'yyyyMMdd') + N'.bak';
     SET @SQL = N'BACKUP DATABASE ' + QUOTENAME(@DBName) + 
@@ -27,7 +27,13 @@ BEGIN
                N' SKIP, NOREWIND, NOUNLOAD, COMPRESSION, ' + 
                N' STATS = 20, MAXTRANSFERSIZE = 4194304, BUFFERCOUNT = 8;';
    
-    EXEC sp_executesql @SQL;
+    BEGIN TRY
+        EXEC sp_executesql @SQL;
+    END TRY
+    BEGIN CATCH
+        SET @Mes = N'Error backing up ' + @DBName + N': ' + ERROR_MESSAGE();
+        RAISERROR(@Mes, 10, 1) WITH NOWAIT;
+    END CATCH
 
     SET @Mes = N'---' + @DBName + N' END---';
     RAISERROR(@Mes, 10, 1) WITH NOWAIT;
